eae86149b78cb76d7614066b8e75ebcabfb83232
todorpopov <todorpopov02@gmail.com>
2025-03-13 01:24:14 +0200

    Add Spring Boot to API module

---
+++ api/src/main/java/com/medrec/auth/AuthController.java
@@ -0,0 +1,14 @@
+package com.medrec.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @GetMapping()
+    public String auth() {
+        return "Hello World";
+    }
+}
